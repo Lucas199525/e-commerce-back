cb13fcf16270d346e9ae1835cacbbec3dcc103eb
Lsiani <lsiani@flowbe.fr>
2025-01-22 10:30:21 +0100

    init add security

---
+++ src/main/java/com/ArcomTech/ApiV1/controller/Auth.java
@@ -0,0 +1,5 @@
+package com.ArcomTech.ApiV1.controller;
+
+public class Auth {
+
+}
---
+++ src/main/java/com/ArcomTech/ApiV1/controller/UserAuthController.java
@@ -0,0 +1,48 @@
+package com.ArcomTech.ApiV1.controller;
+
+@RestController
+@RequestMapping("/auth")
+public class UserAuthController {
+    @Autowired
+    private UserInfoService service;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/user/userProfile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String userProfile() {
+        return "Welcome to User Profile";
+    }
+
+    @GetMapping("/admin/adminProfile")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminProfile() {
+        return "Welcome to Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
