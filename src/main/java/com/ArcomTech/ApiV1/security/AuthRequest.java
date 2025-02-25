32e4d8d649af88835473aef2fc1add51f4c69bf7
Lucas Siani <70410261+Lucas199525@users.noreply.github.com>
2025-02-25 22:58:31 +0100

    add auth

---
+++ src/main/java/com/ArcomTech/ApiV1/security/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.ArcomTech.ApiV1.security;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
